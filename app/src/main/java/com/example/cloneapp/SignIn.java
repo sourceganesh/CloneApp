507e7542ea819b8bdce138895f700036fb7091c9
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-11 18:47:10 +0530

    0.01

--- app/src/main/java/com/example/cloneapp/SignIn.java
+++ app/src/main/java/com/example/cloneapp/SignIn.java
@@ -3,6 +3,14 @@ package com.example.cloneapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseEncoder;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class SignIn extends AppCompatActivity {
 
@@ -12,4 +20,25 @@ public class SignIn extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+
+    
+    public void callClick(View Click){
+        final ParseObject newClick = new ParseObject("Test Save");
+        newClick.put("Name","Ganesh");
+        Toast.makeText(SignIn.this,"Called",Toast.LENGTH_SHORT).show();
+        newClick.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null)
+                    Toast.makeText(SignIn.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(SignIn.this, e.getMessage() + "", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+
+    
+
 }
